1381293f619521f8f60da811cea8541f75bea2b9
eurdream98 <eurdream99@naver.com>
2023-12-28 17:17:53 +0900

    2023-12-28 chap01-literal-and variable-homework

---
+++ src/com/ohgiraffers/levle01/basic/Application2.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.levle01.basic;
+
+public class Application2 {
+    public static void main(String[] args) {
+        double width = 12.5;
+        double height = 36.4;
+
+        System.out.println("면적 : "+width*height);
+        System.out.println("둘레 : "+2*(width+height));
+    }
+}
